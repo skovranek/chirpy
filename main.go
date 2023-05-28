72f1a5f5df0b56d7f284d2791cc519256125bdf5
skovranek <mattskov1@gmail.com>
2023-05-28 03:17:42 -0400

    add handler for path '/'

--- main.go
+++ main.go
@@ -7,15 +7,13 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
 	CORSmux := middlewareCORS(mux)
-
 	svr := http.Server{
 		Handler: CORSmux,
 		Addr: "localhost:8080",
 	}
-
 	svr.ListenAndServe()
-
 }
 
 func middlewareCORS(next http.Handler) http.Handler {
@@ -32,24 +30,26 @@ func middlewareCORS(next http.Handler) http.Handler {
 }
 
 /*
+serves a file called index.html
+[x] Add the HTML code above to a file called index.html in the same root directory as your server
+[x] Use the http.NewServeMux's .Handle() method to add a handler for the root path (/).
+[ ] Use a standard http.FileServer as the handler
+[ ] Use http.Dir to convert a filepath, (in our case a dot: . which indicates the current directory) to a directory for the http.FileServer.
+
+[ ] Re-build and run your server
+[ ] Test your server by visiting http://localhost:8080 in your browser
+[ ] Run the tests in the window on the right
+
+----------------------------------------------------
 run a server that binds to localhost:8080 and always responds with a 404 Not Found response.
 [x] Create a new http.ServeMux
 [x] Wrap that mux in a custom middleware function that adds CORS headers to the response
 	(see the tip below on how to do that).
 [x] Create a new http.Server and use the corsMux as the handler
 [x] Use the server's ListenAndServe method to start the server
-[ ] Build and run your server (e.g. go build -o out && ./out)
-[ ] Open http://localhost:8080 in your browser. You should see a 404 error because we haven't connected any handler logic yet. Don't worry, that's what is expected for the tests to pass for now.
-[ ] Run the tests in the window on the right.
-
-You might have a browser setting that blocks JS/WASM requests to localhost.
-If so, you'll need to disable it or use a different browser.
-Known issues exist with:
---Brave browser's "shield"
---Google Chrome > settings > advanced > privacy and security > site settings >
-	insecure content > ensure "block insecure private network requests" is disabled
-
-or the Boot.dev test suite to be able to make requests to your fileserver,
+[x] Build and run your server (e.g. go build -o out && ./out)
+[x] Open http://localhost:8080 in your browser. You should see a 404 error because we haven't connected any handler logic yet. Don't worry, that's what is expected for the tests to pass for now.
+[x] Run the tests in the window on the right.
 you'll need to add a few headers to all of your responses:
 Access-Control-Allow-Origin: *
 You'll also need to handle requests with the HTTP OPTIONS method
