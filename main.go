263fc97ca08de6279e79ba2a8d81cac905ac9b94
skovranek <mattskov1@gmail.com>
2023-06-20 14:35:29 -0400

    remove comment

--- main.go
+++ main.go
@@ -5,25 +5,6 @@ import (
 	"net/http"
 )
 
-/*
-TODO:
-[x] fix lack of auth headers causing index panic when len called on missing param string
-[x] get rid of get user by ID, redundant since maps return key
-[x] refactor
-[x] extract
-[x] extract server
-[x] extract auth
-[x] extract in db
-[x] remove JWT and bcrypt from database
-[x] errors fmt
-[x] change constants to env_vars
-
-[x] add mux Rlock defer Runlock
-[ ] add documentation to readme
-[ ] add markdown to readme
-[ ] push to github
-*/
-
 func main() {
 	apiCfg := configure()
 
